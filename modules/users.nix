7b0c6d828e50b832b1b54b9a2877e5dfaea8790a
Keith Calvelli <keith@calvelli.dev>
2025-10-26 20:10:20 -0400

    Remove conflicting home-manager settings from users.nix
    users.nix was setting:
      useGlobalPkgs = false
      useUserPackages = true
    
    But system/default.nix now sets both to true for overlay support.
    
    This caused a conflict:
      'The option home-manager.useGlobalPkgs has conflicting definition values'
    
    Fix: Remove these settings from users.nix since they're now centrally
    configured in system/default.nix. This ensures custom packages from
    the axios overlay are available everywhere.
    
    Fixes rebuild switch error on existing systems.

--- modules/users.nix
+++ modules/users.nix
@@ -3,8 +3,7 @@
   # Configure home-manager for user configurations
   # User accounts are defined in your config repo via userModulePath parameter
   home-manager = {
-    useGlobalPkgs = false; # Use separate nixpkgs instance for home-manager
-    useUserPackages = true; # Install user packages directly to the user's profile
+    # useGlobalPkgs and useUserPackages are set in system/default.nix
     extraSpecialArgs = {
       inherit inputs self;
     };
