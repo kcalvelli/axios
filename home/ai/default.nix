bc4a8a5967bebed2dc55d16f0131c488536fad34
Keith Calvelli <keith@calvelli.dev>
2025-11-11 16:20:12 -0500

    Adding google jules via npm

--- home/ai/default.nix
+++ home/ai/default.nix
@@ -7,11 +7,9 @@
     ./mcp.nix
   ];
 
-  config = lib.mkIf config.osConfig.services.ai.enable {
-    home.activation.installJules = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      $DRY_RUN_CMD npm install -g @google/jules
-    '';
-  };
+  home.activation.installJules = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    $DRY_RUN_CMD npm install -g @google/jules
+  '';
 
   # Additional AI configuration can go here
 }
